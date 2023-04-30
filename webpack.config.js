870f8dc3f184cbb80c9b1345a0fcf61432f61697
Maira Medrano <mairamedrano28@gmail.com>
2023-04-29 21:35:36 -0500

    Fixed: wtvr

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const webpack = require('webpack');
+
 
 module.exports = {
     entry: "./client/src/index.js",
